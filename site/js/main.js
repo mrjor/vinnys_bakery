a42d429dff31ebd7517fd4e558fb407a01bec005
joran veenstra <joranveenstra@gmail.com>
2014-06-15 16:08:31 +0200

    productpagina tpegevoegd

--- site/js/main.js
+++ site/js/main.js
@@ -1,1 +1,18 @@
+$('.item').on('click',function(e){
+	e.stopPropagation();
+	var elm = $(this);
+	if(!elm.hasClass('closed')) return;
+	elm.removeClass('closed');
+	elm.find('.info').height('auto');
+	var height = elm.find('.info').height();
+	//console.log(height)
+	elm.find('.info').height(0);
+	elm.find('.info').height(height);
+});
 
+$('.info-close').on('click',function(e){
+	e.stopPropagation();
+	if($(this).parent().hasClass('closed')) return;
+	$(this).parent().addClass('closed');
+	$(this).parent().find('.info').height(0)
+});
